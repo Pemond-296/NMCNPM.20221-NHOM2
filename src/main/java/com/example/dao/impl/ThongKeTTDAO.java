1338b7744627345780291cfb4634922f9e3d1b39
Vuz <vunguyenaz1309@gmail.com>
2023-02-13 23:44:12 +0700

    update TaoMinhChung

--- src/main/java/com/example/dao/impl/ThongKeTTDAO.java
+++ src/main/java/com/example/dao/impl/ThongKeTTDAO.java
@@ -11,12 +11,12 @@ public class ThongKeTTDAO extends AbstractDAO<ThongKeTTModel> implements IThongK
     @Override
     public List<ThongKeTTModel> findByDip(EventGiftModel e) {
         StringBuilder sql = new StringBuilder("select ten_chu_ho, ten_phan_thuong, so_luong, (gia_tri * so_luong) as tien");
-        sql.append(" from dip inner join qua on dip.idQua = qua.id");
-        sql.append(" inner join MinhChung mc on mc.idDip = dip.idDip");
+        sql.append(" from Dip inner join qua on Dip.idQua = qua.id");
+        sql.append(" inner join MinhChung mc on mc.idDip = Dip.idDip");
         sql.append(" inner join TraoThuong tt on tt.idMinhChung = mc.idMinhChung");
         sql.append(" inner join nhankhau nk on nk.id = mc.idNhanKhau");
         sql.append(" inner join hokhau hk on hk.id = nk.id_hokhau");
-        sql.append(" where dip.idDip = ?");
+        sql.append(" where Dip.idDip = ?");
 
         return query(sql.toString(), new ThongKeTTMapper(), e.getId());
     }
